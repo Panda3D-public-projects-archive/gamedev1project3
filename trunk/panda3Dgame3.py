075029911db24d1212183c68aa965b3822265be0
eliztowns@gmail.com <eliztowns@gmail.com@ab9ebedf-ff26-f143-4d03-c03792a7ae08>
2011-10-30 18:04:18 +0000

    basic

---
+++ trunk/panda3Dgame3.py
@@ -0,0 +1,101 @@
+import direct.directbase.DirectStart #starts Panda
+from pandac.PandaModules import *    #basic Panda modules
+from direct.showbase.DirectObject import DirectObject  #for event handling
+from direct.actor.Actor import Actor #for animated models
+from direct.interval.IntervalGlobal import *  #for compound intervals
+from direct.task import Task         #for update fuctions
+import sys, math, random
+
+class World(object): #subclassing here is necessary to accept events
+    def __init__(self):
+        #turn off mouse control, otherwise camera is not repositionable
+        base.disableMouse()
+        camera.setPosHpr(0, -15, 7, 0, -15, 0)
+        self.loadModels()
+        self.setupLights()
+        self.setupCollisions()
+        render.setShaderAuto() #you probably want to use this
+        self.keyMap = {"left":0, "right":0, "forward":0}
+        self.accept("escape", sys.exit)
+        
+        ############################################
+        ## Class code - example
+        # taskMgr.add(self.move, "moveTask")
+        # self.prevtime = 0
+        # self.isMoving = False
+        # self.accept("escape", sys.exit)
+        # self.accept("arrow_up", self.setKey, ["forward", 1])
+        # self.accept("arrow_right", self.setKey, ["right", 1])
+        # self.accept("arrow_left", self.setKey, ["left", 1])
+        # self.accept("arrow_up-up", self.setKey, ["forward", 0])
+        # self.accept("arrow_right-up", self.setKey, ["right", 0])
+        # self.accept("arrow_left-up", self.setKey, ["left", 0])
+        # self.accept("ate-smiley", self.eat)
+        
+        ## "mouse1" is the event when the left mouse button is clicked
+        ## append "-up" for the equivalent of a pygame.KEYUP event
+        #######################################################
+     
+    def setKey(self, key, value):
+        self.keyMap[key] = value     
+        
+    def loadModels(self):
+        """loads models into the world"""
+        #load models here - see panda code for examples
+        
+    def	setupLights(self):
+        #ambient light
+        self.ambientLight = AmbientLight("ambientLight")
+        #four values, RGBA (alpha is largely irrelevent), value range is 0:1
+        self.ambientLight.setColor((.25, .25, .25, 1))
+        self.ambientLightNP = render.attachNewNode(self.ambientLight)
+        #the nodepath that calls setLight is what gets illuminated by the light
+        render.setLight(self.ambientLightNP)
+        #call clearLight() to turn it off
+        
+        
+        ##################################################
+        ## Class code - example
+        # self.keyLight = DirectionalLight("keyLight")
+        # self.keyLight.setColor((.6,.6,.6, 1))
+        # self.keyLightNP = render.attachNewNode(self.keyLight)
+        # self.keyLightNP.setHpr(0, -26, 0)
+        # render.setLight(self.keyLightNP)
+        # self.fillLight = DirectionalLight("fillLight")
+        # self.fillLight.setColor((.4,.4,.4, 1))
+        # self.fillLightNP = render.attachNewNode(self.fillLight)
+        # self.fillLightNP.setHpr(30, 0, 0)
+        # render.setLight(self.fillLightNP)
+        ######################################################
+        
+        
+    def setupCollisions(self):
+        #instantiates a collision traverser and sets it to the default
+        base.cTrav = CollisionTraverser()
+        self.cHandler = CollisionHandlerEvent()
+        #set pattern for event sent on collision
+        # "%in" is substituted with the name of the into object
+        self.cHandler.setInPattern("collide-%in")
+        
+        ##################################################################
+        ## Class code - example
+        # cSphere = CollisionSphere((0,0,0), 500) #because the panda is scaled way down
+        # cNode = CollisionNode("panda")
+        # cNode.addSolid(cSphere)
+        # cNode.setIntoCollideMask(BitMask32.allOff()) #panda is *only* a from object
+        # cNodePath = self.panda.attachNewNode(cNode)
+        ##cNodePath.show()
+        ##registers a from object with the traverser with a corresponding handler
+        # base.cTrav.addCollider(cNodePath, self.cHandler)
+        
+        # for target in self.targets:
+            # cSphere = CollisionSphere((0,0,0), 2)
+            # cNode = CollisionNode("smiley")
+            # cNode.addSolid(cSphere)
+            # cNodePath = target.attachNewNode(cNode)
+            ##cNodePath.show()
+        
+
+w = World()
+run()
+
