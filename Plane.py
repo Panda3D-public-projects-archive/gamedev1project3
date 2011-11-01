81e4a0059df1c832fd1e7ae03a29e0d34f1c4249
eliztowns@gmail.com <eliztowns@gmail.com>
2011-11-01 22:15:58 +0000

    PANDA

---
+++ Plane.py
@@ -0,0 +1,50 @@
+from direct.showbase.DirectObject import DirectObject  #for event handling
+from direct.actor.Actor import Actor #for animated models
+from direct.task import Task
+import sys, math, random
+from direct.interval.IntervalGlobal import * #for compound intervals
+from pandac.PandaModules import * #basic Panda modules
+   
+class Plane(DirectObject):
+    def __init__(self):
+        #load model
+        self.plane = Actor("models/panda-model", {"walk":"panda-walk4"})
+        self.plane.setScale(.005)
+        self.plane.setH(180)
+        self.plane.reparentTo(render)
+        taskMgr.add(self.move, "moveTask")
+        self.keyMap = {"left":0, "right":0, "forward":0}
+        self.prevtime = 0
+        self.isMoving = False
+        
+        
+    def setKey(self,key,value):
+        self.keyMap[key] = value
+    
+    def move(self, task):
+        elapsed = task.time - self.prevtime
+        camera.lookAt(self.plane)
+        if self.keyMap["left"]:
+            self.plane.setH(self.plane.getH() + elapsed * 100)
+        if self.keyMap["right"]:
+            self.plane.setH(self.plane.getH() - elapsed * 100)
+        if self.keyMap["forward"]:
+            dist = 8 * elapsed
+            angle = deg2Rad(self.plane.getH())
+            dx = dist * math.sin(angle)
+            dy = dist * -math.cos(angle)
+            self.plane.setPos(self.plane.getX() + dx, self.plane.getY() + dy, 0)
+            
+        if self.keyMap["left"] or self.keyMap["right"] or self.keyMap["forward"]:
+            if self.isMoving == False:
+                self.isMoving = True
+                self.plane.loop("walk")
+        else:
+            if self.isMoving:
+                self.isMoving = False
+                self.plane.stop()
+                self.plane.pose("walk", 4)
+        
+        self.prevtime = task.time
+        return Task.cont
+        
